260ed2a7c338fcbb955384cfc68916ed5f52b957
d491405879 <d491405879@gmail.com>
2019-10-28 09:25:38 +0200

    fix homepage

---
+++ build/precache-manifest.98f42f1adff299f5b67c49320dfbef4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407f200235f58c3bf54aa457d02fb1e5",
+    "url": "/react-basement/build/index.html"
+  },
+  {
+    "revision": "2ac31d462be9508c79ce",
+    "url": "/react-basement/build/static/css/main.f12bc23e.chunk.css"
+  },
+  {
+    "revision": "43776e07a489a559a3e9",
+    "url": "/react-basement/build/static/js/2.c29d736f.chunk.js"
+  },
+  {
+    "revision": "2ac31d462be9508c79ce",
+    "url": "/react-basement/build/static/js/main.3b3e2044.chunk.js"
+  },
+  {
+    "revision": "95c559cf0a843982f119",
+    "url": "/react-basement/build/static/js/runtime~main.70810711.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f3719aa3394fe45844174a07038916f.js"
+  "/react-basement/build/precache-manifest.98f42f1adff299f5b67c49320dfbef4b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-basement/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
